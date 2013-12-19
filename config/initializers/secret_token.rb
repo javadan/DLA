403fe104a0cea0b8a30c8024c03ab2bd22dc7d0f
Dan Davey <whoisdanieldavey@gmail.com>
2013-12-19 02:43:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DirtyLoveAffairs::Application.config.secret_key_base = 'eb30f4313b6afee38af9f6877b978096312a61356f144f77e7e4ec76ad1de0e0199cb7d1dc1eaa736fb96828f14e47c7d54e57afcdfd090511c825f6e0f546f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DirtyLoveAffairs::Application.config.session_store :cookie_store, key: '_DirtyLoveAffairs_session'
